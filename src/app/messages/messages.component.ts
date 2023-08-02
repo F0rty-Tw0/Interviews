841f54cd6f5d07d050cc28eb24939ca4b16cabe9
imperfectdeals <admin@imperfectdeals.com>
2023-08-02 11:59:13 +0200

    question

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -4,10 +4,9 @@ import { MessageService } from '../message.service';
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.css']
+  styleUrls: ['./messages.component.css'],
+  providers: [MessageService],
 })
 export class MessagesComponent {
-
   constructor(public messageService: MessageService) {}
-
 }
